89392b7c9ecc4f6dc5a13d6eca5b72c9f2db8f67
Cosmin POP <mtx.rake@gmail.com>
2022-05-12 08:44:39 +0300

    initial tokens

---
+++ utils/tokens.lua
@@ -0,0 +1,31 @@
+TALC_TOKENS = {
+    [40610] = { name = "Chestguard of the Lost Conqueror",
+                classes = "Paladin Priest Warlock",
+                rewards = {
+                    [39497] = "Warlock",
+                    [39515] = "Priest",
+                    [39523] = "Priest",
+                    [39629] = "Paladin",
+                    [39633] = "Paladin",
+                    [39638] = "Paladin",
+                }
+    },
+
+    [40613] = { name = "Gloves of the Lost Conqueror", classes = "Paladin Priest Warlock" },
+
+    [40616] = { name = "Helm of the Lost Conqueror", classes = "Paladin Priest Warlock" },
+
+    [40619] = { name = "Leggings of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+
+    [40622] = { name = "Spaulders of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+
+    [40625] = { name = "Breastplate of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+
+    [40628] = { name = "Gauntlets of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+
+    [40631] = { name = "Crown of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+
+    [40634] = { name = "Legplates of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+
+    [40637] = { name = "Mantle of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+} 
