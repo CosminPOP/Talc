3877400cd6e21a2416f3a166f5cffd102917e409
Cosmin POP <mtx.rake@gmail.com>
2022-05-17 20:57:43 +0300

    added some more items

--- utils/tokens.lua
+++ utils/tokens.lua
@@ -1,31 +1,111 @@
 TALC_TOKENS = {
-    [40610] = { name = "Chestguard of the Lost Conqueror",
-                classes = "Paladin Priest Warlock",
-                rewards = {
-                    [39497] = "Warlock",
-                    [39515] = "Priest",
-                    [39523] = "Priest",
-                    [39629] = "Paladin",
-                    [39633] = "Paladin",
-                    [39638] = "Paladin",
-                }
+    [44569] = {
+        name = "The Key to the Focusing Iris",
+        rewards = { 44582 }
+    },
+    [40610] = {
+        name = "Chestguard of the Lost Conqueror",
+        rewards = { 39497, 39515, 39523, 39629, 39633, 39638, }
+    },
+    [40613] = {
+        name = "Gloves of the Lost Conqueror",
+        rewards = { 39500, 39519, 39530, 39632, 39634, 39639 }
+    },
+    [40616] = {
+        name = "Helm of the Lost Conqueror",
+        rewards = { 39496, 39514, 39521, 39628, 39635, 39640 }
+    },
+    [40619] = {
+        name = "Leggings of the Lost Conqueror",
+        rewards = { 39498, 39517, 39528, 39630, 39636, 39641 }
+    },
+    [40622] = {
+        name = "Spaulders of the Lost Conqueror",
+        rewards = { 39499, 39518, 39529, 39631, 39637, 39642 }
+    },
+    [40625] = {
+        name = "Breastplate of the Lost Conqueror",
+        rewards = { 40423, 40449, 40458, 40569, 40574, 40579 }
+    },
+    [40628] = {
+        name = "Gauntlets of the Lost Conqueror",
+        rewards = { 40420, 40445, 40454, 40570, 40575, 40580 }
+    },
+    [40631] = {
+        name = "Crown of the Lost Conqueror",
+        rewards = { 40421, 40447, 40456, 40571, 40576, 40581 }
+    },
+    [40634] = {
+        name = "Legplates of the Lost Conqueror",
+        rewards = { 40422, 40448, 40457, 40572, 40577, 40583 }
+    },
+    [40637] = {
+        name = "Mantle of the Lost Conqueror",
+        rewards = { 40424, 40450, 40459, 40573, 40578, 40584 }
     },
 
-    [40613] = { name = "Gloves of the Lost Conqueror", classes = "Paladin Priest Warlock" },
+    [40611] = {
+        name = "Chestguard of the Lost Protector",
+        rewards = { 39579, 39588, 39592, 39597, 39606, 39611 }
+    },
 
-    [40616] = { name = "Helm of the Lost Conqueror", classes = "Paladin Priest Warlock" },
 
-    [40619] = { name = "Leggings of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
+    [40614] = {
+        name = "Gloves of the Lost Protector",
+        rewards = { 39582, 39591, 39593, 39601, 39609, 39622 }
+    },
+    --[000] = {
+    --    name = "Helm of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Leggings of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Spaulders of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Breastplate of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Gauntlets of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Crown of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Legplates of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
+    --[000] = {
+    --    name = "Mantle of the Lost Protector",
+    --    rewards = {
+    --
+    --    }
+    --},
 
-    [40622] = { name = "Spaulders of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
-
-    [40625] = { name = "Breastplate of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
-
-    [40628] = { name = "Gauntlets of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
-
-    [40631] = { name = "Crown of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
-
-    [40634] = { name = "Legplates of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
-
-    [40637] = { name = "Mantle of the Lost Conqueror ", classes = "Paladin Priest Warlock" },
-} 
+    -- quest items
+    [46052] = {
+        name = "Reply-Code Alpha", -- 10man
+        rewards = { 46320, 46321, 46322, 46323 }
+    }
+}
