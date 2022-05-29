a44f3928e9a08fb63621e0fba79c0d4c85b20ef2
Cosmin POP <mtx.rake@gmail.com>
2022-05-29 16:12:18 +0300

    add all naxx 10 add heroic key to iris add fragment of valanyr

--- utils/tokens.lua
+++ utils/tokens.lua
@@ -1,111 +1,72 @@
 TALC_TOKENS = {
+    -- Phase 1
+
     [44569] = {
         name = "The Key to the Focusing Iris",
         rewards = { 44582 }
     },
-    [40610] = {
-        name = "Chestguard of the Lost Conqueror",
-        rewards = { 39497, 39515, 39523, 39629, 39633, 39638, }
-    },
-    [40613] = {
-        name = "Gloves of the Lost Conqueror",
-        rewards = { 39500, 39519, 39530, 39632, 39634, 39639 }
-    },
-    [40616] = {
-        name = "Helm of the Lost Conqueror",
-        rewards = { 39496, 39514, 39521, 39628, 39635, 39640 }
-    },
-    [40619] = {
-        name = "Leggings of the Lost Conqueror",
-        rewards = { 39498, 39517, 39528, 39630, 39636, 39641 }
-    },
-    [40622] = {
-        name = "Spaulders of the Lost Conqueror",
-        rewards = { 39499, 39518, 39529, 39631, 39637, 39642 }
-    },
-    [40625] = {
-        name = "Breastplate of the Lost Conqueror",
-        rewards = { 40423, 40449, 40458, 40569, 40574, 40579 }
-    },
-    [40628] = {
-        name = "Gauntlets of the Lost Conqueror",
-        rewards = { 40420, 40445, 40454, 40570, 40575, 40580 }
-    },
-    [40631] = {
-        name = "Crown of the Lost Conqueror",
-        rewards = { 40421, 40447, 40456, 40571, 40576, 40581 }
-    },
-    [40634] = {
-        name = "Legplates of the Lost Conqueror",
-        rewards = { 40422, 40448, 40457, 40572, 40577, 40583 }
-    },
-    [40637] = {
-        name = "Mantle of the Lost Conqueror",
-        rewards = { 40424, 40450, 40459, 40573, 40578, 40584 }
+    [44577] = {
+        name = "Heroic Key to the Focusing Iris",
+        rewards = { 44581 }
     },
 
-    [40611] = {
-        name = "Chestguard of the Lost Protector",
-        rewards = { 39579, 39588, 39592, 39597, 39606, 39611 }
-    },
+    -- t8 10man
+
+    [40610] = { name = "Chestguard of the Lost Conqueror", rewards = { 39497, 39515, 39523, 39629, 39633, 39638, } },
+    [40625] = { name = "Breastplate of the Lost Conqueror", rewards = { 40423, 40449, 40458, 40569, 40574, 40579 } },
+
+    [40616] = { name = "Helm of the Lost Conqueror", rewards = { 39496, 39514, 39521, 39628, 39635, 39640 } },
+    [40631] = { name = "Crown of the Lost Conqueror", rewards = { 40421, 40447, 40456, 40571, 40576, 40581 } },
+
+    [40613] = { name = "Gloves of the Lost Conqueror", rewards = { 39500, 39519, 39530, 39632, 39634, 39639 } },
+    [40628] = { name = "Gauntlets of the Lost Conqueror", rewards = { 40420, 40445, 40454, 40570, 40575, 40580 } },
+
+    [40619] = { name = "Leggings of the Lost Conqueror", rewards = { 39498, 39517, 39528, 39630, 39636, 39641 } },
+    [40634] = { name = "Legplates of the Lost Conqueror", rewards = { 40422, 40448, 40457, 40572, 40577, 40583 } },
+
+    [40622] = { name = "Spaulders of the Lost Conqueror", rewards = { 39499, 39518, 39529, 39631, 39637, 39642 } },
+    [40637] = { name = "Mantle of the Lost Conqueror", rewards = { 40424, 40450, 40459, 40573, 40578, 40584 } },
 
 
-    [40614] = {
-        name = "Gloves of the Lost Protector",
-        rewards = { 39582, 39591, 39593, 39601, 39609, 39622 }
-    },
-    --[000] = {
-    --    name = "Helm of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Leggings of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Spaulders of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Breastplate of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Gauntlets of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Crown of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Legplates of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
-    --[000] = {
-    --    name = "Mantle of the Lost Protector",
-    --    rewards = {
-    --
-    --    }
-    --},
+    [40611] = { name = "Chestguard of the Lost Protector", rewards = { 39579, 39588, 39592, 39597, 39606, 39611 } },
+    [40626] = { name = "Breastplate of the Lost Protector", rewards = { 40503, 40508, 40514, 40523, 40525, 40544 } },
 
-    -- quest items
+    [40617] = { name = "Helm of the Lost Protector", rewards = { 39578, 39583, 39594, 39602, 39605, 39610 } },
+    [40632] = { name = "Crown of the Lost Protector", rewards = { 40505, 40510, 40516, 40521, 40528, 40546 } },
+
+    [40614] = { name = "Gloves of the Lost Protector", rewards = { 39582, 39591, 39593, 39601, 39609, 39622 } },
+    [40629] = { name = "Gauntlets of the Lost Protector", rewards = { 40504, 40509, 40515, 40520, 40527, 40545 } },
+
+    [40620] = { name = "Leggings of the Lost Protector", rewards = { 39580, 39589, 39595, 39603, 39607, 39612 } },
+    [40635] = { name = "Legplates of the Lost Protector", rewards = { 40506, 40512, 40517, 40522, 40529, 40547 } },
+
+    [40623] = { name = "Spaulders of the Lost Protector", rewards = { 39581, 39590, 39596, 39604, 39608, 39613 } },
+    [40638] = { name = "Mantle of the Lost Protector", rewards = { 40507, 40513, 40518, 40524, 40530, 40548 } },
+
+
+    [40612] = { name = "Chestguard of the Lost Vanquisher", rewards = { 39492, 39538, 39547, 39547, 39558, 39617, 39623 } },
+    [40627] = { name = "Breastplate of the Lost Vanquisher", rewards = { 40418, 40463, 40469, 40471, 40495, 40550, 40559 } },
+
+    [40618] = { name = "Helm of the Lost Vanquisher", rewards = { 39491, 39531, 39545, 39553, 39561, 39619, 39625 } },
+    [40633] = { name = "Crown of the Lost Vanquisher", rewards = { 40416, 40461, 40467, 40473, 40499, 40554, 40565 } },
+
+    [40615] = { name = "Gloves of the Lost Vanquisher", rewards = { 39495, 39543, 39544, 39557, 39560, 39618, 39624 } },
+    [40630] = { name = "Gauntlets of the Lost Vanquisher", rewards = { 40415, 40460, 40466, 40472, 40496, 40552, 40563 } },
+
+    [40621] = { name = "Leggings of the Lost Vanquisher", rewards = { 39493, 39539, 39546, 39555, 39564, 39620, 39626 } },
+    [40636] = { name = "Legplates of the Lost Vanquisher", rewards = { 40417, 40462, 40468, 40493, 40500, 40556, 40567 } },
+
+    [40624] = { name = "Spaulders of the Lost Vanquisher", rewards = { 39494, 39542, 39548, 39556, 39565, 39621, 39627 } },
+    [40639] = { name = "Mantle of the Lost Vanquisher", rewards = { 40419, 40465, 40470, 40494, 40502, 40557, 40568 } },
+
+
+    -- Phase 2
     [46052] = {
         name = "Reply-Code Alpha", -- 10man
         rewards = { 46320, 46321, 46322, 46323 }
-    }
+    },
+    [45038] = {
+        name = "Fragment of Val'anyr",
+        rewards = { 46017 }
+    },
 }
